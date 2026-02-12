e5b5dce20262c23f0793eb4b7f9d9d8c0b3ef46b
elliotpark <esy5kb@virginia.edu>
2026-02-12 14:15:38 -0500

    my work before getContent is made

--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -1,7 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 class UserInterfaceTest {
     @Test
-
+    public void
 }
